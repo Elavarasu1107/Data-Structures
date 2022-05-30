025654d83da0a9441dafeb7c3ec51408d7083594
Elavarasu1107 <sellamuthuappusamy@gmail.com>
2022-05-30 17:44:45 +0530

    [Elavarasu] UC1-Creating LinkedList

---
+++ DataStructures/DataStructures/LinkedList.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    internal class LinkedList
+    {
+        public Node head;
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            if(this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node position = head;
+                while(position.next != null)
+                {
+                    position = position.next;
+                }
+                position.next = node;
+            }
+            Console.WriteLine("{0} inserted into LinkedList" , node.data);
+        }
+        public void Display()
+        {
+            Node position = this.head;
+            if(position == null)
+            {
+                Console.WriteLine("List is Empty");
+                return;
+            }
+            while(position != null)
+            {
+                Console.Write(position.data+" ");
+                position = position.next;
+            }
+        }
+    }
+}
---
+++ DataStructures/DataStructures/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DataStructures
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+            list.Add(50);
+            list.Add(36);
+            list.Add(70);
+            list.Display();
+        }
+    }
+}
